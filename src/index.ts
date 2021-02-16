67a7fe9de95c0d4bf86614311b4a92e338867b77
vaaski <admin@vaa.ski>
2021-02-16 13:02:23 +0100

    feat: export getClientIDv2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import resolve from "./resolve"
 import playlist from "./playlist"
 import user from "./user"
-import { ensureMin } from "./util"
+import { ensureMin, getClientIDv2 } from "./util"
 
-const util = { ensureMin }
+const util = { ensureMin, getClientIDv2 }
 
 export default { resolve, playlist, user, util }
 export { resolve, playlist, user, util }
