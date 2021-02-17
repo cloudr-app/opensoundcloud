7ca6bca29721a0817f06d238452b7571f8946c50
vaaski <admin@vaa.ski>
2021-02-17 09:15:02 +0100

    docs: add favicon and make pwa

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -8,6 +8,25 @@ module.exports = {
     ["meta", { name: "theme-color", content: "#0189E3" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/icons/apple-touch-icon.png" },
+    ],
+    [
+      "link",
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png" },
+    ],
+    [
+      "link",
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png" },
+    ],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#0061d4" }],
+    ["link", { rel: "shortcut icon", href: "/icons/favicon.ico" }],
+    ["meta", { name: "msapplication-TileColor", content: "#0061d4" }],
+    ["meta", { name: "msapplication-config", content: "/icons/browserconfig.xml" }],
+    ["meta", { name: "theme-color", content: "#282c34" }],
+    ["meta", { name: "viewport", content: "width=device-width, initial-scale=1" }],
   ],
   themeConfig: {
     prefersTheme: "dark",
@@ -43,5 +62,15 @@ module.exports = {
       ],
     },
   },
-  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+  plugins: [
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true,
+      },
+    ],
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
+  ],
 }
